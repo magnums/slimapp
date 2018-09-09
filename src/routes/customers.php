5c040bd3ab30c6bd1230e3a1f9e69f8673ac7b46
magnums <satit@wbac.ac.th>
2018-09-09 19:49:34 +0700

    Pretty json format
     Thank you For Kelvao.

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -8,9 +8,13 @@ $app -> options('/{routes:.+}', function($request, $response, $args) {
 	return $response;
 });
 
-$app -> add(function($req, $res, $next) {
-	$response = $next($req, $res);
-	return $response -> withHeader('Access-Control-Allow-Origin', '*') -> withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization') -> withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+$app->add(function ($req, $res, $next) {
+$response = $next($req, $res);
+return $response
+->withHeader('Content-Type', 'application/json')
+->withHeader('Access-Control-Allow-Origin', '*')
+->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
 // Get All Customers
