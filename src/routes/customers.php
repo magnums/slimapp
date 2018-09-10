aa1202be9bcd52716b896621bb8ec94a2e5e7fe8
magnums <satit@wbac.ac.th>
2018-09-10 11:50:17 +0700

    Update customers.php

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -8,6 +8,14 @@ $app -> options('/{routes:.+}', function($request, $response, $args) {
 	return $response;
 });
 
+/*
+//Slipapp original
+$app -> add(function($req, $res, $next) {
+	$response = $next($req, $res);
+	return $response -> withHeader('Access-Control-Allow-Origin', '*') -> withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization') -> withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+*/
+
 $app->add(function ($req, $res, $next) {
 $response = $next($req, $res);
 return $response
